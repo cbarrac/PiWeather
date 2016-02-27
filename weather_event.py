5908d60076710c550ed840e943ae9d8a7a6ea366
Craig Barraclough <craig@barraclough.mobi>
2016-02-27 21:54:48 +1100

    Add configuration support

--- weather_event.py
+++ weather_event.py
@@ -1,116 +1,39 @@
 #!/usr/bin/env python
 
+from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime
-from pywws import DataStore
+import ConfigParser as configparser
 import math
 import numpy
 import sys
 import time
-from apscheduler.schedulers.background import BackgroundScheduler
 
 ########
 # CONFIG
 ########
-DEBUG = 0
-BRUTAL_VIEW = True
-# Output directory for pywws
-STORAGE = "/opt/weather/"
-FORECAST_FILE = "minforecast.txt"
-# Optional Sensors
-BMP085 = False
-BME280 = True
-PISENSE = False
-SI1145 = True
-# Optional Output
-ADA_LCD = True
-CONSOLE_OUTPUT = True
-PISENSE_DISPLAY = False
-MQTT_PUBLISH = True
-# Adafruit LCD
-ADA_LCD_WIDTH = 16
-# SenseHat Display
-ROTATION = 90
-SCROLL = 0.09
-DAWN = 6
-DUSK = 19
-COLOUR_BG = True
-# Colours
-FG = [196,196,0]
-BG = [0,0,64]
-FG_NIGHT = [8,8,0]
-BG_NIGHT = [0,0,0]
-COLOUR_COLD = [0,16,32]
-COLOUR_MID = [0,48,0]
-COLOUR_HOT = [48,0,0]
-COLOUR_COLD_NIGHT = [0,0,7]
-COLOUR_MID_NIGHT = [0,7,0]
-COLOUR_HOT_NIGHT = [7,0,0]
-# Comfort Levels (Indoor Temp)
-COMFORT_LOW = 22
-COMFORT_HIGH = 28
-# Calibration
-# - Altitude above sea level, in metres
-CALIB_ALTITUDE=195
-CALIB_BMP085_PRESSURE=0
-CALIB_BMP085_TEMP_IN=0
-CALIB_BME280_PRESSURE=0
-CALIB_BME280_TEMP_IN=0
-CALIB_BME280_HUM_IN=0
-CALIB_PISENSE_HUM_IN=0
-CALIB_PISENSE_PRESSURE=18.5
-CALIB_PISENSE_TEMP_IN=-2.4
-CALIB_PISENSE_TEMP_OUT=-3.2
-# https://www.adafruit.com/datasheets/Si1145-46-47.pdf
-# Apparently all come with +256 offset
-CALIB_SI1145_VISIBLE=-256
-# Sunlight = 0.282; 2500K Incandescent = 0.319; "Cool white" flourescent = 0.146
-# At gain 0, in High Signal Range mode "High Signal Range (Gain divided by 14.5)"
-CALIB_SI1145_VISIBLE_RESPONSE=(0.282 / 14.5)
-# Apparently all come with +256 offset
-CALIB_SI1145_IR=-256
-# Sunlight = 2.44; 2500K Incandescent=8.46; "Cool white" flourescent=0.71
-# At gain 0, in High Signal Range mode "High Signal Range (Gain divided by 14.5)"
-CALIB_SI1145_IR_RESPONSE=(2.44 / 14.5)
-CALIB_SI1145_UV=0
-# Estimated transmission of current glass covering
-CALIB_SI1145_UV_RESPONSE=0.55
-# Event Periods / Timers
-ADALCD_OUTPUT_RATE = 15
-CONSOLE_OUTPUT_RATE = 60
-FLUSH_RATE = 180
-FORECAST_REFRESH_RATE = 300
-MQTT_OUTPUT_RATE = 60
-SAMPLE_RATE = 10
-SENSEHAT_OUTPUT_RATE = 30
-STORE_RATE = 60
-# Smoothing over ... values
-SMOOTHING = 30
-# Magic Values (used for 'empty')
-MININT = -(sys.maxsize - 1)
-# MQ Connectivity
-MQTT_SERVER = "hab1.internal"
-MQTT_PORT = 1883
-MQTT_CLIENTID = "piweather"
-MQTT_PREFIX = "/sensors/"
+config = configparser.ConfigParser()
+config.read('PiWeather.ini')
 
 ########
 # Optional
 ########
-if ADA_LCD:
+if config.getboolean('Output','ADA_LCD'):
 	import Adafruit_CharLCD
 	AdaLcd = Adafruit_CharLCD.Adafruit_CharLCDPlate()
-if BME280:
+if config.getboolean('Sensors','BME280'):
 	from Adafruit_BME280 import *
 	BmeSensor = BME280(mode=BME280_OSAMPLE_8)
-if BMP085:
+if config.getboolean('Sensors','BMP085'):
 	import Adafruit_BMP.BMP085 as BMP085
 	BmpSensor = BMP085.BMP085()
-if MQTT_PUBLISH:
+if config.getboolean('Output','MQTT_PUBLISH'):
 	import paho.mqtt.publish as publish
-if PISENSE or PISENSE_DISPLAY:
+if config.getboolean('Output','PYWWS_PUBLISH'):
+	from pywws import DataStore
+if config.getboolean('Sensors','SENSEHAT') or config.getboolean('Output','SENSEHAT_DISPLAY'):
 	from sense_hat import SenseHat
-	PiSense = SenseHat()
-if SI1145:
+	PiSenseHat = SenseHat()
+if config.getboolean('Sensors','SI1145'):
 	import SI1145.SI1145 as SI1145
 	SiSensor = SI1145.SI1145()
 
@@ -128,7 +51,7 @@ def AltitudeOffset(Altitude):
 	return (s - p) / 100
 
 def Debug(message):
-	if DEBUG == 1:
+	if config.getint('General','DEBUG') == 1:
 		print message
 
 def DewPoint(RH,TempC):
@@ -160,10 +83,8 @@ def Flush(ds,dstatus):
 
 def ForecastRefresh():
 	Debug("ForecastRefresh: start")
-	if FORECAST_REFRESH_RATE == 0:
-		return
 	global forecast
-	forecast_file = STORAGE + FORECAST_FILE
+	forecast_file = config.get('ForecastFile','FILE')
 	try:
 		with open(forecast_file) as f:
 			forecast = f.read()
@@ -199,7 +120,7 @@ def MqClose():
 
 def MqInit():
     global mqtt_client
-    mqtt_client.connect(MQTT_SERVER, MQTT_PORT, 60)
+    mqtt_client.connect(config.get('MQTT','SERVER'), config.getint('MQTT','PORT'), config.getint('MQTT','TIMEOUT'))
     Debug("MqInit: Starting loop")
     mqtt_client.loop_start()
 
@@ -207,20 +128,20 @@ def MqSendMultiple():
 	Debug("MqSendMultiple: Build Message")
 	msgs = []
 	for reading in readings:
-		mq_path = MQTT_PREFIX + reading
+		mq_path = config.get('MQTT','PREFIX') + reading
 		value = readings[reading][0]
 		msg = {'topic':mq_path,'payload':value}
 		Debug("MqSendMultiple: Payload Element {0}".format(msg))
 		msgs.append(msg)
 	Debug("MqSendMultiple: Sending multiple")
 	try:
-		publish.multiple(msgs,hostname=MQTT_SERVER,port=MQTT_PORT,client_id=MQTT_CLIENTID)
+		publish.multiple(msgs,hostname=config.get('MQTT','SERVER'),port=config.getint('MQTT','PORT'),client_id=config.get('MQTT','CLIENTID'))
 	except:
 		Debug("Error sending MQTT message")
 	Debug("MqSendMultiple: Complete")
 
 def MqSendSingle(variable,value):
-    mq_path = MQTT_PREFIX + variable
+    mq_path = config.get('MQTT','PREFIX') + variable
     Debug("MqSendSingle: Sending {0} = {1:0.1f}".format(mq_path,value))
     mqtt_client.publish(mq_path, value)
 
@@ -231,39 +152,38 @@ def RelToAbsHumidity(relativeHumidity, temperature):
 def Sample():
 	Debug("Sample: read")
 	global readings
-	if BME280:
+	if config.getboolean('Sensors','BME280'):
 		# !Make sure to read temperature first!
 		# !The library sets OverSampling and waits for valid values _only_ in the read_raw_temperature function!
 		try:
-			Smoothing('temp_in', (BmeSensor.read_temperature() + CALIB_BME280_TEMP_IN))
+			Smoothing('temp_in', (BmeSensor.read_temperature() + config.getfloat('Calibration','BME280_TEMP_IN')))
 			# Note: read_pressure returns Pa, divide by 100 for hectopascals (hPa)
-			Smoothing('abs_pressure', ((BmeSensor.read_pressure()/100) + CALIB_ALTITUDE_PRESSURE_OFFSET + CALIB_BME280_PRESSURE))
-			Smoothing('hum_in', (BmeSensor.read_humidity() + CALIB_BME280_HUM_IN))
+			Smoothing('abs_pressure', ((BmeSensor.read_pressure()/100.0) + config.getfloat('Calibration','ALTITUDE_PRESSURE_OFFSET') + config.getfloat('Calibration','BME280_PRESSURE')))
+			Smoothing('hum_in', (BmeSensor.read_humidity() + config.getfloat('Calibration','BME280_HUM_IN')))
 		except:
 			Debug("Error reading BME280")
-	if BMP085:
+	if config.getboolean('Sensors','BMP085'):
 		try:
-			Smoothing('temp_in', (BmpSensor.read_temperature() + CALIB_BMP085_TEMP_IN))
+			Smoothing('temp_in', (BmpSensor.read_temperature() + config.getfloat('Calibration','BMP085_TEMP_IN')))
 			# Note: read_pressure returns Pa, divide by 100 for hectopascals (hPa)
-			Smoothing('abs_pressure', ((BmpSensor.read_pressure()/100) + CALIB_BMP085_PRESSURE))
+			Smoothing('abs_pressure', ((BmpSensor.read_pressure()/100.0) + config.getfloat('Calibration','BMP085_PRESSURE')))
 		except:
 			Debug("Error reading BMP085")
-	if PISENSE:
+	if config.getboolean('Sensors','SENSEHAT'):
 		try:
-			Smoothing('abs_pressure', (PiSense.get_pressure() + CALIB_PISENSE_PRESSURE))
-			Smoothing('hum_in', (PiSense.get_humidity() + CALIB_PISENSE_HUM_IN))
-			Smoothing('temp_in', (PiSense.get_temperature_from_pressure() + CALIB_PISENSE_TEMP_IN))
-			#Smoothing('temp_out', (PiSense.get_temperature_from_humidity() + CALIB_PISENSE_TEMP_OUT))
+			Smoothing('abs_pressure', (PiSenseHat.get_pressure() + config.getfloat('Calibration','ALTITUDE_PRESSURE_OFFSET') + config.getfloat('Calibration','SENSEHAT_PRESSURE')))
+			Smoothing('hum_in', (PiSenseHat.get_humidity() + config.getfloat('Calibration','SENSEHAT_HUM_IN')))
+			Smoothing('temp_in', (PiSenseHat.get_temperature_from_pressure() + config.getfloat('Calibration','SENSEHAT_TEMP_IN')))
 		except:
-			Debug("Error reading PISENSE")
-	if SI1145:
+			Debug("Error reading SENSEHAT")
+	if config.getboolean('Sensors','SI1145'):
 		try:
-			Smoothing('illuminance', ((SiSensor.readVisible() + CALIB_SI1145_VISIBLE) / CALIB_SI1145_VISIBLE_RESPONSE))
-			Smoothing('ir', ((SiSensor.readIR() + CALIB_SI1145_IR) / CALIB_SI1145_IR_RESPONSE))
-			Smoothing('uv', (((SiSensor.readUV()/100.0) + CALIB_SI1145_UV) / CALIB_SI1145_UV_RESPONSE))
+			Smoothing('illuminance', ((SiSensor.readVisible() + config.getfloat('Calibration','SI1145_VISIBLE')) / config.getfloat('Calibration','SI1145_VISIBLE_RESPONSE')))
+			Smoothing('ir', ((SiSensor.readIR() + config.getfloat('Calibration','SI1145_IR')) / config.getfloat('Calibration','SI1145_IR_RESPONSE')))
+			Smoothing('uv', (((SiSensor.readUV()/100.0) + config.getfloat('Calibration','SI1145_UV')) / config.getfloat('Calibration','SI1145_UV_RESPONSE')))
 		except:
 			Debug("Error reading SI1145")
-	if BME280 or BMP085 or PISENSE:
+	if config.getboolean('Sensors','BME280') or config.getboolean('Sensors','BMP085') or config.getboolean('Sensors','SENSEHAT'):
 		try:
 			Smoothing('dew_point_in',DewPoint(readings['hum_in'][0],readings['temp_in'][0]))
 		except:
@@ -279,15 +199,15 @@ def Smoothing(channel, value):
 	global readings
 	if readings.get(channel,None) is None:
 		Debug("Init %s" % channel)
-		readings[channel] = [MININT for x in xrange(SMOOTHING+1)]
-	for i in range(1,(SMOOTHING)):
-		if readings[channel][i+1] == MININT:
+		readings[channel] = [config.getint('General','MININT') for x in xrange(config.getint('General','SMOOTHING')+1)]
+	for i in range(1,(config.getint('General','SMOOTHING'))):
+		if readings[channel][i+1] == config.getint('General','MININT'):
 			readings[channel][i+1] = value
 		readings[channel][i] = readings[channel][i+1]
 		average += readings[channel][i]
-	readings[channel][SMOOTHING] = value
+	readings[channel][config.getint('General','SMOOTHING')] = value
 	average += value
-	average = average / SMOOTHING
+	average = average / config.getint('General','SMOOTHING')
 	readings[channel][0] = average
 	Debug("Smoothing: Readings[%s]: %s" % (channel, readings[channel]))
 	Debug("Smoothing: Complete")
@@ -356,7 +276,7 @@ def WriteAdaLcd():
 			msg = "{0:0.1f}C {1:0.0f}% UV:{2:0.1f}\n{3:0.1f}hPa".format(readings['temp_in'][0],readings['hum_in'][0],readings['uv'][0],readings['abs_pressure'][0])
 			AdaScreenNumber = 1
 		elif AdaScreenNumber == 1:
-			msg = FormatDisplay(forecast, ADA_LCD_WIDTH)
+			msg = FormatDisplay(forecast, config.getint('Adafruit_LCD','LCD_WIDTH'))
 			AdaScreenNumber = 0
 		else:
 			msg = "No message"
@@ -372,19 +292,19 @@ def WriteAdaLcd():
 		uv = readings['uv'][0]
 		if uv < 3.0:
 			# Low
-			AdaLcd.set_color(0.0,1.0,0.0)	#rgb(0,255,0)
+			AdaLcd.set_color(0.3,1.0,0.3)	#rgb(64,255,64)
 		elif uv < 6.0:
 			# Moderate
-			AdaLcd.set_color(1.0,1.0,0.0)	#rgb(255,255,0)
+			AdaLcd.set_color(1.0,1.0,0.3)	#rgb(255,255,64)
 		elif uv < 6.0:
 			# High
-			AdaLcd.set_color(1.0,0.5,0.0)	#rgb(255,128,0)
+			AdaLcd.set_color(1.0,0.5,0.3)	#rgb(255,128,64)
 		elif uv < 11.0:
 			# Very High
-			AdaLcd.set_color(1.0,0.0,0.0)	#rgb(255,0,0)
+			AdaLcd.set_color(1.0,0.3,0.3)	#rgb(255,64,64)
 		else:
 			# Extreme
-			AdaLcd.set_color(1.0,0.0,1.0)	#rgb(255,0,255)
+			AdaLcd.set_color(1.0,0.3,1.0)	#rgb(255,64,255)
 	except:
 		Debug("WriteAdaLcd: Error setting backlight")
 	try:
@@ -395,7 +315,7 @@ def WriteAdaLcd():
 def WriteConsole():
 	Debug("WriteConsole: start")
 	print time.ctime(),
-	if BRUTAL_VIEW:
+	if config.getboolean('Output','BRUTAL_VIEW'):
 		for reading in readings:
 			value = readings[reading][0]
 			print "{0}: {1:.1f}".format(reading,value),
@@ -442,7 +362,7 @@ def WriteConsole():
 def WriteSenseHat():
 	Debug("WriteSenseHat: start")
 	global forecast_toggle
-	if FORECAST_REFRESH_RATE > 0 and forecast_toggle == 1 and forecast:
+	if config.getint('Rates','FORECAST_REFRESH_RATE') > 0 and forecast_toggle == 1 and forecast:
 		forecast_toggle = 0
 		msg = forecast
 	else:
@@ -452,33 +372,31 @@ def WriteSenseHat():
 		except:
 			msg = "Awaiting data"
 	hour = datetime.now().hour
-	if hour > DAWN and hour < DUSK:
-		PiSense.low_light = False
-		Foreground = FG
-		if COLOUR_BG:
-			Background = COLOUR_MID
-			if readings['temp_in'][0] <=COMFORT_LOW:
-				Background = COLOUR_COLD
-			elif readings['temp_in'][0] > COMFORT_HIGH:
-				Background = COLOUR_HOT
+	if hour > config.getint('General','DAWN') and hour < config.getint('General','DUSK'):
+		PiSenseHat.low_light = False
+		Foreground = config.get('SenseHat','FG')
+		if config.getboolean('SenseHat','COLOUR_BG'):
+			Background = config.get('SenseHat','COLOUR_MID')
+			if readings['temp_in'][0] <= config.get('SenseHat','COMFORT_LOW'):
+				Background = config.get('SenseHat','COLOUR_COLD')
+			elif readings['temp_in'][0] > config.get('SenseHat','COMFORT_HIGH'):
+				Background = config.get('SenseHat','COLOUR_HOT')
 		else:
-			Background = BG
+			Background = config.get('SenseHat','BG')
 	else:
-		PiSense.low_light = True
-		Foreground = FG_NIGHT
-		if COLOUR_BG:
-			Background = COLOUR_MID_NIGHT
-			if readings['temp_in'][0] < COMFORT_LOW:
-				Background = COLOUR_COLD_NIGHT
-			if readings['temp_in'][0] > COMFORT_HIGH:
-				Background = COLOUR_HOT_NIGHT
+		PiSenseHat.low_light = True
+		Foreground = config.get('SenseHat','FG_NIGHT')
+		if config.getboolean('SenseHat','COLOUR_BG'):
+			Background = config.get('SenseHat','COLOUR_MID_NIGHT')
+			if readings['temp_in'][0] < config.get('SenseHat','COMFORT_LOW'):
+				Background = config.get('SenseHat','COLOUR_COLD_NIGHT')
+			if readings['temp_in'][0] > config.get('SenseHat','COMFORT_HIGH'):
+				Background = config.get('SenseHat','COLOUR_HOT_NIGHT')
 		else:
-			Background = BG_NIGHT
-	PiSense.show_message(msg, scroll_speed=SCROLL, text_colour=Foreground, back_colour=Background)
-	PiSense.clear()
+			Background = config.get('SenseHat','BG_NIGHT')
+	PiSenseHat.show_message(msg, scroll_speed=config.get('SenseHat','SCROLL'), text_colour=Foreground, back_colour=Background)
+	PiSenseHat.clear()
 	Debug("WriteSenseHat: Complete")
-
-
 
 
 ########
@@ -492,15 +410,20 @@ forecast_toggle = 0
 global_init=True
 readings = {}
 # pywws data
-ds = DataStore.data_store(STORAGE)
-dstatus = DataStore.status(STORAGE)
-if ADA_LCD:
+if config.getboolean('Output','PYWWS_PUBLISH'):
+	ds = DataStore.data_store(config.get('PYWWS','STORAGE'))
+	dstatus = DataStore.status(config.get('PYWWS','STORAGE'))
+if config.getboolean('Output','ADA_LCD'):
 	AdaLcd.clear()
-if PISENSE_DISPLAY:
+if config.getboolean('Output','SENSEHAT_DISPLAY'):
 	# Set up display
-	PiSense.clear()
-	PiSense.set_rotation(ROTATION)
-CALIB_ALTITUDE_PRESSURE_OFFSET = AltitudeOffset(CALIB_ALTITUDE)
+	PiSenseHat.clear()
+	PiSenseHat.set_rotation(config.get('SenseHat','ROTATION'))
+try:
+	config.getfloat('Calibration','ALTITUDE_PRESSURE_OFFSET')
+except:
+	PressureOffset = AltitudeOffset(config.getint('Calibration','ALTITUDE'))
+	config.set('Calibration','ALTITUDE_PRESSURE_OFFSET', PressureOffset)
 # Warm up sensors
 print "Waiting for sensors to settle"
 for i in range(1,6):
@@ -511,25 +434,27 @@ ForecastRefresh()
 Sample()
 print "Scheduling events..."
 scheduler = BackgroundScheduler()
-scheduler.add_job(Sample, 'interval', seconds=SAMPLE_RATE, id='Sample')
-scheduler.add_job(Store, 'interval', seconds=STORE_RATE, id='Store', args=[ds])
-scheduler.add_job(Flush, 'interval', seconds=FLUSH_RATE, id='Flush', args=[ds,dstatus])
-scheduler.add_job(ForecastRefresh, 'interval', seconds=FORECAST_REFRESH_RATE, id='Forecast')
-if ADA_LCD:
-	scheduler.add_job(WriteAdaLcd, 'interval', seconds=ADALCD_OUTPUT_RATE, id='AdaLcd')
-if CONSOLE_OUTPUT:
-	scheduler.add_job(WriteConsole, 'interval', seconds=CONSOLE_OUTPUT_RATE, id='Console')
-if MQTT_PUBLISH:
-	scheduler.add_job(MqSendMultiple,'interval',seconds=MQTT_OUTPUT_RATE,id='MQTT')
-if PISENSE_DISPLAY:
-	scheduler.add_job(WriteSenseHat, 'interval', seconds=SENSEHAT_OUTPUT_RATE, id='SenseHat')
+scheduler.add_job(Sample, 'interval', seconds=config.getint('Rates','SAMPLE_RATE'), id='Sample')
+if config.getboolean('Output','PYWWS_PUBLISH'):
+	scheduler.add_job(Store, 'interval', seconds=config.getint('Rates','STORE_RATE'), id='Store', args=[ds])
+	scheduler.add_job(Flush, 'interval', seconds=config.getint('Rates','FLUSH_RATE'), id='Flush', args=[ds,dstatus])
+if config.getboolean('Sensors','FORECAST_FILE'):
+	scheduler.add_job(ForecastRefresh, 'interval', seconds=config.getint('Rates','FORECASTFILE_REFRESH_RATE'), id='Forecast')
+if config.getboolean('Output','ADA_LCD'):
+	scheduler.add_job(WriteAdaLcd, 'interval', seconds=config.getint('Rates','ADALCD_OUTPUT_RATE'), id='AdaLcd')
+if config.getboolean('Output','CONSOLE_OUTPUT'):
+	scheduler.add_job(WriteConsole, 'interval', seconds=config.getint('Rates','CONSOLE_OUTPUT_RATE'), id='Console')
+if config.getboolean('Output','MQTT_PUBLISH'):
+	scheduler.add_job(MqSendMultiple,'interval',seconds=config.getint('Rates','MQTT_OUTPUT_RATE'),id='MQTT')
+if config.getboolean('Output','SENSEHAT_DISPLAY'):
+	scheduler.add_job(WriteSenseHat, 'interval', seconds=config.getint('Rates','SENSEHAT_OUTPUT_RATE'), id='SenseHat')
 
 scheduler.start()
-if ADA_LCD:
+if config.getboolean('Output','ADA_LCD'):
 	WriteAdaLcd()
-if CONSOLE_OUTPUT:
+if config.getboolean('Output','CONSOLE_OUTPUT'):
 	WriteConsole()
-if MQTT_PUBLISH:
+if config.getboolean('Output','MQTT_PUBLISH'):
 	MqSendMultiple()
 print "Entering event loop"
 
@@ -541,11 +466,12 @@ except (KeyboardInterrupt, SystemExit):
 	# Not strictly necessary if daemonic mode is enabled but should be done if possible
 	print "Shutting down scheduler"
 	scheduler.shutdown()
-	print "Flushing data"
-	ds.flush()
-	dstatus.flush()
-	if ADA_LCD:
+	if config.getboolean('Output','PYWWS_PUBLISH'):
+		print "Flushing data"
+		ds.flush()
+		dstatus.flush()
+	if config.getboolean('Output','ADA_LCD'):
 		AdaLcd.clear()
-	if PISENSE or PISENSE_DISPLAY:
-		PiSense.clear()
+	if config.getboolean('Output','SENSEHAT_DISPLAY'):
+		PiSenseHat.clear()
 	print "Goodbye"
